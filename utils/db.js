40b5f4a46337ddf562ab9a31f93ccfb18c6bfb98
Vanshmakhija16 <133850728+Vanshmakhija16@users.noreply.github.com>
2025-09-02 18:43:58 +0000

    change to railwaysql

--- utils/db.js
+++ utils/db.js
@@ -1,15 +1,12 @@
-import mysql from "mysql2";
+import mysql from 'mysql2'
 
 const pool = mysql.createPool({
-  host: "turntable.proxy.rlwy.net",
-  port: 34639,
-  user: "root",
-  password: "fildFMUpDWUToKxvNgGXktXxfcHXTuWq",
-  database: "railway"
+    host: "mysql.railway.internal",
+    user: "root",
+    password: "fildFMUpDWUToKxvNgGXktXxfcHXTuWq",
+    database: "railway",
+    connectionLimit: 10
 });
-
-
-
 
 pool.getConnection((err, connection) => {
     if (err) {
